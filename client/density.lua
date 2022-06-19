d86cb22da1cb18d138fb87a0c634162e15ef30f8
MGC DEV <85084072+Mountiesdevelopment@users.noreply.github.com>
2022-06-19 09:31:12 -0700

    Update density.lua

--- client/density.lua
+++ client/density.lua
@@ -1,6 +1,6 @@
 local density = {
-    ['parked'] = 0.8,
-    ['vehicle'] = 0.8,
+    ['parked'] = 0.85,
+    ['vehicle'] = 0.85,
     ['multiplier'] = 0.8,
     ['peds'] = 0.8,
     ['scenario'] = 0.8,
